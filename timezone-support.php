29c728d9db9bac47bc675f770fd751f5ed3946da
Jeff Higham <jeff@f3code.com>
2020-10-09 09:57:56 -0400

    Added ACF configuration directly to the plugin.

--- timezone-support.php
+++ timezone-support.php
@@ -98,3 +98,61 @@ function eea_timezone_filter($ics_data, $datetime)
 
   return $ics_data;
 }
+
+
+if( function_exists('acf_add_local_field_group') ):
+
+  acf_add_local_field_group(array(
+    'key' => 'group_5f80498f3a1bd',
+    'title' => 'EE Timezone Support',
+    'fields' => array(
+      array(
+        'key' => 'field_5f8049b23f138',
+        'label' => 'Event Timezone',
+        'name' => 'event_timezone',
+        'type' => 'select',
+        'instructions' => 'Select the timezone you would like this event in.',
+        'required' => 0,
+        'conditional_logic' => 0,
+        'wrapper' => array(
+          'width' => '',
+          'class' => '',
+          'id' => '',
+        ),
+        'choices' => array(
+          'pacific' => 'Pacific',
+          'mountain' => 'Mountain',
+          'central' => 'Central',
+          'eastern' => 'Eastern',
+        ),
+        'default_value' => array(
+          0 => 'mountain',
+        ),
+        'allow_null' => 0,
+        'multiple' => 0,
+        'ui' => 0,
+        'return_format' => 'value',
+        'ajax' => 0,
+        'placeholder' => '',
+      ),
+    ),
+    'location' => array(
+      array(
+        array(
+          'param' => 'post_type',
+          'operator' => '==',
+          'value' => 'espresso_events',
+        ),
+      ),
+    ),
+    'menu_order' => 0,
+    'position' => 'normal',
+    'style' => 'seamless',
+    'label_placement' => 'top',
+    'instruction_placement' => 'label',
+    'hide_on_screen' => '',
+    'active' => true,
+    'description' => '',
+  ));
+  
+  endif;
